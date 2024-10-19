2f893babf4d6313cfa6d553504fb464a0beeaba1
Sim Juhyun <154858191+JuhyunSim@users.noreply.github.com>
2024-10-19 12:00:54 +0900

    feat front-chat-create&read 채팅기능 화면 구현 - 게시물 조회 -> 과외문의하기 -> 메시지 전송 -> 채팅방 생성 -> 채팅방목록조회 -> 채팅방
    기타
    - 디렉토리 구조 변경

--- src/main/java/com/simzoo/withmedical/repository/chat/room/ChatRoomRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.simzoo.withmedical.repository.chat.room;
-
-import com.simzoo.withmedical.dto.chat.ChatRoomSimpleResponseDto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface ChatRoomRepositoryCustom {
-    Page<ChatRoomSimpleResponseDto> findAllChatRoomsCreatedByMe(Long userId, Pageable pageable);
-}
