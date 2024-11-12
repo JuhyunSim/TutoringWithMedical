13d12346aa7a3d755e8664230d39df65669466a5
Sim Juhyun <154858191+JuhyunSim@users.noreply.github.com>
2024-11-12 18:02:06 +0900

    feat-back-review-002 리뷰 CRUD 버그 수정 & 토큰 설정
    리뷰
    수정, 삭제 시 자기 리뷰에 대해서만 호출 가능하도록 수정
    - ReviewController
      - updateReview : LoginId 어노테이션 추가, userId를 서비스코드에서 활용
      - deleteReview
        - 오타수정
        - LoginId 어노테이션 추가, 서비스코드에 userId 전달
    - ReviewRepository
      - findByIdAndWriter_Id 추가
    - ReviewService
      - changeReview : ReviewRepository.findById -> ReviewRepository.findByIdAndWriter_Id 수정
      - deleteReview : ReviewRepository.findById -> ReviewRepository.findByIdAndWriter_Id 수정
    
    한 튜터에게는 사용자 당 1개 리뷰
    - ReviewService
      - ReviewRepository.findByTutorProfile_IdAndWriter_Id 추가. writer_id는 userId
    
    리뷰 데이터 저장 시 데이터베이스 버그
    : memberEntity의 memberId가 unique 제한에 위배되면서 에러 발생
    - ReviewEntity
      - MemberEntity 와 관계 수정(OneToOne -> ManyToOne)
    
    리뷰 데이터 반환값 수정
    - ReviewResponseDto
      - tutorNickname 삭제
      - reviewId 추가
    - ReviewRepositoryCustomImpl
      - findReviewsByTutorId에서 dto 생성자 수정(review.id 추가)
    기타
    - build.gradle : swagger 설정
    - ErrorCode : ALREADY_EXIST_REVIEW 추가

--- backend/src/main/java/com/simzoo/withmedical/exception/ErrorCode.java
+++ backend/src/main/java/com/simzoo/withmedical/exception/ErrorCode.java
@@ -22,7 +22,8 @@ public enum ErrorCode {
     DECRYPTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "복호화 과정에서 문제가 발생했습니다."),
     ENCRYPTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "암호화 과정에서 문제가 발생했습니다."),
     UNAUTHENTICATED_USER(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
-    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.")
+    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    ALREADY_EXIST_REVIEW(HttpStatus.UNAUTHORIZED, "이미 작성한 리뷰가 있습니다.")
     ;
 
     private HttpStatus httpStatus;
--- backend/src/main/java/com/simzoo/withmedical/repository/review/ReviewRepositoryCustomImpl.java
+++ backend/src/main/java/com/simzoo/withmedical/repository/review/ReviewRepositoryCustomImpl.java
@@ -1,38 +1,45 @@
 package com.simzoo.withmedical.repository.review;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.simzoo.withmedical.dto.QReviewResponseDto;
 import com.simzoo.withmedical.dto.ReviewResponseDto;
+import com.simzoo.withmedical.entity.QReviewEntity;
+import com.simzoo.withmedical.util.OrderSpecifierUtil;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
+@RequiredArgsConstructor
 public class ReviewRepositoryCustomImpl implements ReviewRepositoryCustom{
 
-    JPAQueryFactory queryFactory;
+    private final JPAQueryFactory queryFactory;
 
     @Override
     public Page<ReviewResponseDto> findReviewsByTutorId(Long tutorId, Pageable pageable) {
 
-//        QReviewEntity review = QReviewEntity.reviewEntity;
-//
-//        Long total = queryFactory.select(review.count())
-//            .from(review)
-//            .where(review.tutorProfile.id.eq(tutorId))
-//            .fetchOne();
-//
-//        List<ReviewResponseDto> reviews = queryFactory.select(new QReviewResponseDto(
-//            review.tutorProfile.member.nickname,
-//            review.writer.nickname,
-//            review.rating,
-//            review.content,
-//            review.createdAt,
-//            review.updatedAt))
-//            .from(review)
-//            .where(review.tutorProfile.id.eq(tutorId))
-//            .orderBy(OrderSpecifierUtil.getAllOrderSpecifiers(pageable, "reviewEntity"))
-//            .offset(pageable.getOffset())
-//            .limit(pageable.getPageSize())
-//            .fetch();
+        QReviewEntity review = QReviewEntity.reviewEntity;
 
-        return null;
+        Long total = queryFactory.select(review.count())
+            .from(review)
+            .where(review.tutorProfile.id.eq(tutorId))
+            .fetchOne();
+
+        List<ReviewResponseDto> reviews = queryFactory.select(new QReviewResponseDto(
+            review.id,
+            review.writer.nickname,
+            review.rating,
+            review.content,
+            review.createdAt,
+            review.updatedAt))
+            .from(review)
+            .where(review.tutorProfile.id.eq(tutorId))
+            .orderBy(OrderSpecifierUtil.getAllOrderSpecifiers(pageable, "reviewEntity"))
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+        return new PageImpl<>(reviews, pageable, total);
     }
 }
